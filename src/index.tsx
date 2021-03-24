b7ee65edb247a7adeb5735cb03511df3f9e4e9ef
Bianca Gualter <bncgualter@gmail.com>
2021-03-24 13:42:06 -0300

    🚧  listing transactions and seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id:1,
+          title: 'Freelance de website',
+          type:'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-19 13:00:00')
+        },
+        {
+          id:2,
+          title: 'Açaí',
+          type:'withdraw',
+          category: 'Alimentação',
+          amount: 25,
+          createdAt: new Date('2021-02-23 18:00:00')
+        },
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api'
     
