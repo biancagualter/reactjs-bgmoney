c5ba3e9e675127058fdeb4cc00ccaf76b48dc32f
Bianca Gualter <bncgualter@gmail.com>
2021-03-24 02:38:10 -0300

    🚧  creating Type buttons

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,7 +12,6 @@ export const GlobalStyle = createGlobalStyle `
         --text-title: #363F5F;
         --text-body: #969CB3;
 
-        --background: #F0F2F5;
         --shape: #FFFFFF;
     }
 
@@ -38,7 +37,7 @@ export const GlobalStyle = createGlobalStyle `
     }
 
     body, input, textarea, button {
-        font-family: 'Popins', sans-serif;
+        font-family: 'Poppins', sans-serif;
         font-weight: 400;
     }
 
